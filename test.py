c59a5081948c16ffaa8019e6ee3eaf9495f1ab8a
User <xxx>
2022-07-15 11:31:43 +0800

    Add a test image

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
   model = torch.nn.DataParallel(model, device_ids=[0]).cuda()
   function.load_pre_weights(model, "weights.pth")
 
-  image_path = ""
+  image_path = "pics/00001.tif"
   image = cv2.imread(image_path)
   mask_pred = function.inference(model, image)
   visualize.display(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), None, mask_pred)
